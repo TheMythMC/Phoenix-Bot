3d02a3bb32b56cc94cecbb627f6200600435ac4d
BananasAmIRite <jasony1230@gmail.com>
2021-03-04 21:14:38 -0500

    Removed debug variables

--- src/Commands/CoreCommands/Misc/TestCommand.js
+++ src/Commands/CoreCommands/Misc/TestCommand.js
@@ -1,40 +1,5 @@
 const Command = require("../../../Structure/Command");
 
-const GuildRole = require("../../../RoleSync/RoleTemplates/GuildRole"); 
-
-const Rank = require("../../../RoleSync/RoleTemplates/Rank"); 
-const RankSync = require("../../../RoleSync/RoleSync"); 
-
-const { performance } = require("perf_hooks"); 
-
-const ranks = [ 
-    {
-        DiscordRoleID: "817204448631062558", 
-        RoleTemplate: "Rank", 
-        Params: ["MVP_PLUS_PLUS"]
-    }, 
-    {
-        DiscordRoleID: "817188603360444416", 
-        RoleTemplate: "Rank", 
-        Params: ["MVP_PLUS"]
-    }, 
-    {
-        DiscordRoleID: "817204540851879967", 
-        RoleTemplate: "Rank", 
-        Params: ["MVP"]
-    }, 
-    {
-        DiscordRoleID: "817204579434627072", 
-        RoleTemplate: "Rank", 
-        Params: ["VIP_PLUS"]
-    }, 
-    {
-        DiscordRoleID: "817204614701121558", 
-        RoleTemplate: "Rank", 
-        Params: ["VIP"]
-    }, 
-]; 
-
 class TestCommand extends Command {
     constructor(client) {
         super(client, "testcommand", {
