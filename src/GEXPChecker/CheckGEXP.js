fdb57663f4fe470fee81cfce8d1d554d75ce5b04
BananasAmIRite <jasony1230@gmail.com>
2021-03-12 22:31:16 -0500

    added checkgexp and a lotta todo's

--- src/GEXPChecker/CheckGEXP.js
+++ src/GEXPChecker/CheckGEXP.js
@@ -1,4 +1,42 @@
-module.exports = async (client, guild) => {
+const HypixelAPI = require("../Structure/HypixelAPI"); 
+
+module.exports = async (client, guild, guildCheckDays) => {
     const g = await client.Bot.GuildManager.getGuild(guild.id); 
-    const data = await g.data.GEXPData;  
+    if (!g || !g.data.GuildID) return {}; 
+    const data = await g.data.GEXPData; 
+    
+    // get data for guild
+    const guildData = HypixelAPI.getGuildDataByName(g.data.GuildID); // TODO: replace with getGuildDataByID, pretend g.data.GuildID is guild name for now
+    
+    let res = {}; 
+
+    // please optimize this looks so ugly i hate it but idk how to optimize  - Banana
+    for (member of guildData.members) {
+        let gexp = 0; 
+        const xpHistory = Object.values(member.exp_history);
+        for (let i = 0; i < guildCheckDays; i++) { // standard for loop cuz of the guildCheckDays
+            if (!xpHistory[i]) break; 
+            gexp += xpHistory[i]; 
+        }
+
+        const roleReq = data.find(d => d.RoleName === member.rank);
+        if (!roleReq) continue; 
+        res[member.uuid] = { // TODO: change to displaying name instead of UUID
+            Rank: member.rank, 
+            Gexp: gexp, 
+            Passed: gexp >= roleReq.MinExp 
+        }
+    }
+
+    /*
+    
+    {
+        "MemberName: String": {
+            Rank: String, 
+            Gexp: Integer, 
+            Passed: Boolean
+        }
+    }
+    
+    */
 }
--- src/Structure/HypixelAPI.js
+++ src/Structure/HypixelAPI.js
@@ -2,16 +2,21 @@ const Hypixel = require('phoenix-slothpixel');
 const slothpixelURL = process.env.SLOTHPIXEL_URL;
 
 async function getPlayerData(playerName) {
-    return await Hypixel(`players/${playerName}`, slothpixelURL || process.env.SLOTHPIXEL_URL);
+    return await Hypixel(`players/${playerName}`, slothpixelURL);
 }
 
 async function getGuildDataByName(guildName) {
 
-    return await Hypixel(`guilds/name/${guildName}`, slothpixelURL || process.env.SLOTHPIXEL_URL);
+    return await Hypixel(`guilds/name/${guildName}`, slothpixelURL);
 }
 
 async function getGuildDataByPlayer(playerName) {
-    return await Hypixel(`guilds/${playerName}`, slothpixelURL || process.env.SLOTHPIXEL_URL);
+    return await Hypixel(`guilds/${playerName}`, slothpixelURL);
+}
+
+// NOT IMPLEMENTED
+async function getGuildDataByID(id) {
+    return await Hypixel(`guilds/id/${id}`, slothpixelURL); // TODO: yo myth please implement this :D
 }
 
 module.exports = { getGuildDataByName, getGuildDataByPlayer, getPlayerData }
