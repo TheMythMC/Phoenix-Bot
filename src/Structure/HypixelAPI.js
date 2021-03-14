bcab60f3bdcfaedd65c03033d230451324f86599
Adam Reisenauer <atr10605@hotmail.com>
2021-03-14 12:08:54 -0400

    Fixed slothpixel url, removed mojang api & updated depends, added Mojang API

--- src/Structure/HypixelAPI.js
+++ src/Structure/HypixelAPI.js
@@ -1,22 +1,23 @@
 const Hypixel = require('phoenix-slothpixel');
 const slothpixelURL = process.env.SLOTHPIXEL_URL;
+const backupSlothpixelURL = 'https:///api.slothpixel.me/api'
 
 async function getPlayerData(playerName) {
-    return await Hypixel(`players/${playerName}`, slothpixelURL || 'https://api.slothpixel.net/api');
+    return await Hypixel(`players/${playerName}`, slothpixelURL || backupSlothpixelURL);
 }
 
 async function getGuildDataByName(guildName) {
 
-    return await Hypixel(`guilds/name/${guildName}`, slothpixelURL || 'https://api.slothpixel.net/api');
+    return await Hypixel(`guilds/name/${guildName}`, slothpixelURL || backupSlothpixelURL);
 }
 
 async function getGuildDataByPlayer(playerName) {
-    return await Hypixel(`guilds/${playerName}`, slothpixelURL || 'https://api.slothpixel.net/api');
+    return await Hypixel(`guilds/${playerName}`, slothpixelURL || backupSlothpixelURL);
 }
 
 // NOT IMPLEMENTED
 async function getGuildDataByID(id) {
-    return await Hypixel(`guilds/id/${id}`, slothpixelURL || 'https://api.slothpixel.net/api'); // TODO: yo myth please implement this :D
+    return await Hypixel(`guilds/id/${id}`, slothpixelURL || backupSlothpixelURL); // TODO: yo myth please implement this :D
 }
 
 module.exports = { getGuildDataByName, getGuildDataByPlayer, getPlayerData }
