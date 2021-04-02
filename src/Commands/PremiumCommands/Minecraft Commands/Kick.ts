9ce76062ea5a2537e7c20e5b922c371db2190633
Mythbusters123 <atr10605@hotmail.com>
2021-04-01 21:48:07 -0400

    Add mute command (vscode be dumbo)

---
+++ src/Commands/PremiumCommands/Minecraft Commands/Kick.ts
@@ -0,0 +1,23 @@
+import { Message } from 'discord.js';
+import BotCore from '../../../Structure/BotCore';
+import Command from '../../../Structure/Command';
+import Util from '../../../utils/Util'
+import { Bot } from 'mineflayer';
+import { sendSuccessMessage } from '../../../utils/MessageUtils';
+
+module.exports = class Kick extends Command {
+    constructor(client: BotCore) {
+        super(client, 'kick', {
+            description: 'Kicks a player from the guild',
+            usage: '%pkick <Player Name>',
+            category: 'Minecraft Commands',
+            requiredPerms: ['ADMINISTRATOR']
+        });
+    }
+
+    async run(message: Message, args: string[], client: BotCore) {
+        let mcBot: Bot = Util.getMinecraftBotForGuild(message.guild.id);
+        mcBot.chat(`\/kick ${args[0]}`);
+        sendSuccessMessage(message.channel, `Kicked ${args[0]}`)
+    }
+}
