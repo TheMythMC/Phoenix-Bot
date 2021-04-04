d67d45e5361d3221cd4e82b1620ddc5406c72d95
BananasAmIRite <jasony1230@gmail.com>
2021-04-04 12:03:04 -0400

    fixed time.ts (probvs doesnt amtter)

--- src/express/routers/live/time.ts
+++ src/express/routers/live/time.ts
@@ -1,19 +1,14 @@
-import express from 'express'
-const router = express.Router(); 
+import express from "express";
+const router = express.Router();
 
 router.ws("/", (ws, req) => {
-    
-    let e; 
-    ws.on('open', () => {
-        e = setInterval(() => {
-            ws.send(Date.now()); 
-        }, 1000); 
-    })
-    
+  let e = setInterval(() => {
+    ws.send(Date.now());
+  }, 1000);
 
-    ws.on('close', () => {
-        clearInterval(e); 
-    }); 
-}); 
+  ws.on("close", () => {
+    clearInterval(e);
+  });
+});
 
-module.exports = router; 
+module.exports = router;
