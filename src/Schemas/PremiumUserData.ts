6ac7e9e4fb6db60944f432ecbae45c9c43301412
BananasAmIRite <jasony1230@gmail.com>
2021-04-06 13:12:08 -0400

    added custom prefix data

--- src/Schemas/PremiumUserData.ts
+++ src/Schemas/PremiumUserData.ts
@@ -1,16 +1,25 @@
 import mongoose from "mongoose";
 
+type PrefixStatus = "BEDWARS_LEVEL" | "SKYWARS_LEVEL" | "BEDWARS_COINS" | "SKYWARS_COINS" | "SKYBLOCK_COINS"; // add for even more support for games
+type PrefixTypes = { [key in PrefixStatus]: string };
+
 export interface IPremiumUserData extends mongoose.Document {
   DiscordID: string;
+  CustomPrefixData: PrefixTypes;
 }
 
 export const schema = new mongoose.Schema({
   DiscordID: String,
+  CustomPrefixData: {
+    name: String,
+    values: String,
+  },
 });
 
 const model = mongoose.model<IPremiumUserData>("PremiumUserData", schema);
 const Default = {
   DiscordID: "",
+  CustomPrefixData: {},
 };
 export const createDefault = (DiscordID: string) => {
   let obj = {} as IPremiumUserData;
