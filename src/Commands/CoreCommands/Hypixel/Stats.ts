0fe65222379b238b30336863df8382eeddba0819
Mythbusters123 <atr10605@hotmail.com>
2021-04-12 18:42:59 -0400

    FINALLY STATS HAS TYPINGS

--- src/Commands/CoreCommands/Hypixel/Stats.ts
+++ src/Commands/CoreCommands/Hypixel/Stats.ts
@@ -4,6 +4,7 @@ import Command from "../../../Structure/Command";
 import aliases from "./gameAliases.json";
 import * as HypixelAPI from "../../../Structure/HypixelAPI";
 import { sendCustomMessage, sendErrorMessage } from "../../../utils/MessageUtils";
+import Player from "phoenix-slothpixel/Player";
 
 module.exports = class extends Command {
   msg: string;
@@ -37,9 +38,9 @@ module.exports = class extends Command {
             ""
           );
         }
-        aliases.games[game].forEach(async (alias) => {
+        aliases.games[game].forEach(async (alias: string) => {
           if (alias === minigame) {
-            await this.parseStats(minigame, HypixelAPI.getPlayerData(playername));
+            await this.parseStats(minigame, await HypixelAPI.getPlayerData(playername));
             return sendCustomMessage(
               message.channel,
               "BLUE",
@@ -51,14 +52,13 @@ module.exports = class extends Command {
         });
       });
     } else {
-      this.parseStats(minigame, HypixelAPI.getPlayerData(playername));
+      this.parseStats(minigame, await HypixelAPI.getPlayerData(playername));
     }
   }
 
-  async parseStats(game: string, data: any) {
+  async parseStats(game: string, data: Player) {
     switch (game) {
       case "all": {
-        
       }
     }
   }
--- src/Structure/HypixelAPI.ts
+++ src/Structure/HypixelAPI.ts
@@ -1,21 +1,20 @@
 import Hypixel from "phoenix-slothpixel";
-import Guild from "phoenix-slothpixel/typings/Guild";
-import Player from "phoenix-slothpixel/typings/Player";
+import Guild from "phoenix-slothpixel/Guild";
+import Player from 'phoenix-slothpixel/Player';
 const slothpixelURL = process.env.SLOTHPIXEL_URL;
-const backupSlothpixelURL = "https://api.slothpixel.me/api";
 
 export async function getPlayerData(playerName: string): Promise<Player> {
   return await Hypixel.playerStats(playerName, slothpixelURL)
 }
 
-export async function getGuildDataByName(guildName): Promise<Guild> {
+export async function getGuildDataByName(guildName: string): Promise<Guild> {
   return await Hypixel.guildStatsByName(guildName, slothpixelURL)
 }
 
-export async function getGuildDataByPlayer(playerName): Promise<Guild> {
+export async function getGuildDataByPlayer(playerName: string): Promise<Guild> {
   return await Hypixel.guildStatsByPlayer(playerName, slothpixelURL)
 }
 
-export async function getGuildDataByID(id): Promise<Guild> {
+export async function getGuildDataByID(id: string): Promise<Guild> {
   return await Hypixel.guildStatsByID(id, slothpixelURL);
 }
