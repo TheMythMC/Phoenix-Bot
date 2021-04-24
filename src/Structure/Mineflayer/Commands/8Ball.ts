183d0d91d583904f7631adcb5625ce5a10097400
Mythbusters123 <atr10605@hotmail.com>
2021-04-24 10:05:21 -0400

    Linted errors

--- src/Structure/Mineflayer/Commands/8Ball.ts
+++ src/Structure/Mineflayer/Commands/8Ball.ts
@@ -7,35 +7,35 @@ module.exports = class extends Command {
     responses: string[];
 	constructor(discordBot: BotCore) {
         super(discordBot, '8ball', {
-            description: "Let the fates decide",
-            usage: "%p8ball <Your question>"
+            description: 'Let the fates decide',
+            usage: '%p8ball <Your question>'
         });
 
         this.responses = [
-            "It is certain.",
-            "Without a doubt.",
-            "You may rely on it",
-            "Yes, definitely.",
-            "It is decidedly so.",
-            "As I see it, yes.",
-            "Most likely.", 
-			"Yes.",
-            "outlook good.",
-            "Signs point to yes",
-            "Reply hazy try again.",
-            "Better not tell you now.",
-            "Ask again later.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-            "Dont count on it.",
-            "Outlook not so good",
-            "My sources say no.",
-            "Very doubtful",
-            "My reply is no."
-        ]
+            'It is certain.',
+            'Without a doubt.',
+            'You may rely on it',
+            'Yes, definitely.',
+            'It is decidedly so.',
+            'As I see it, yes.',
+            'Most likely.', 
+			'Yes.',
+            'outlook good.',
+            'Signs point to yes',
+            'Reply hazy try again.',
+            'Better not tell you now.',
+            'Ask again later.',
+            'Cannot predict now.',
+            'Concentrate and ask again.',
+            'Dont count on it.',
+            'Outlook not so good',
+            'My sources say no.',
+            'Very doubtful',
+            'My reply is no.'
+        ];
     }
 
-    async run(_args: string[], minecraftBot: MineflayerBot, _, _playerName) {
-		minecraftBot.bot.chat(`${_playerName}, ${this.responses[Math.floor(Math.random() * this.responses.length)]}`)
+    async run(_args: string[], minecraftBot: MineflayerBot, _, playerName: string) {
+		minecraftBot.bot.chat(`${playerName}, ${this.responses[Math.floor(Math.random() * this.responses.length)]}`);
     }
-}
+};
