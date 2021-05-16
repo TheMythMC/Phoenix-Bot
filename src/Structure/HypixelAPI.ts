77364a94ac18151f7bb2a1074dbc3774a002cccd
Mythbusters123 <atr10605@hotmail.com>
2021-05-16 09:46:53 -0400

    Revert `HypixelAPI.ts`

--- src/Structure/HypixelAPI.ts
+++ src/Structure/HypixelAPI.ts
@@ -1,8 +1,6 @@
 import Hypixel from 'phoenix-slothpixel';
 import Guild from 'phoenix-slothpixel/Guild';
 import Player from 'phoenix-slothpixel/Player';
-import fetch from 'node-fetch';
-import { getLineAndCharacterOfPosition } from 'typescript';
 
 let slothpixelURL = 'https://api.slothpixel.me/api/';
 
@@ -12,26 +10,26 @@ export function initialize() {
 
 export async function getPlayerData(playerName: string): Promise<Player> {
   // console.log(slothpixelURL);
-  return (await (await fetch(`${slothpixelURL}players/${playerName}`)).json()) as Player;
+  return Hypixel.playerStats(playerName, `${slothpixelURL}players/`);
 }
 
 export async function getGuildDataByName(guildName: string): Promise<Guild> {
-  // return await Hypixel.guildStatsByName(guildName.replace(' ', '+'), `${slothpixelURL}guilds/name/`);
-  return (
-    await (await fetch(`${slothpixelURL}guilds/name/${guildName.replace(' ', '+')}`)).json()
-  ) as Guild;
+  return await Hypixel.guildStatsByName(guildName.replace(' ', '+'), `${slothpixelURL}guilds/name/`);
+  // return (
+  //   await (await fetch(`${slothpixelURL}guilds/name/${guildName.replace(' ', '+')}`)).json()
+  // ) as Guild;
 }
 
 export async function getGuildDataByPlayer(playerName: string): Promise<Guild> {
-  // return await Hypixel.guildStatsByPlayer(playerName, `${slothpixelURL}guilds/`);
-  return (
-    await (await fetch(`${slothpixelURL}guilds/${playerName}`)).json()
-  ) as Guild;
+  return await Hypixel.guildStatsByPlayer(playerName, `${slothpixelURL}guilds/`);
+  // return (
+  //   await (await fetch(`${slothpixelURL}guilds/${playerName}`)).json()
+  // ) as Guild;
 }
 
 export async function getGuildDataByID(id: string): Promise<Guild> {
-  // return await Hypixel.guildStatsByID(id, `${slothpixelURL}guilds/id/`);
-  return (
-    await (await fetch(`${slothpixelURL}guilds/id/${id}`)).json()
-  ) as Guild;
+  return await Hypixel.guildStatsByID(id, `${slothpixelURL}guilds/id/`);
+  // return (
+  //   await (await fetch(`${slothpixelURL}guilds/id/${id}`)).json()
+  // ) as Guild;
 }
